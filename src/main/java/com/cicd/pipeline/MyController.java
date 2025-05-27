26334ced7536e6ed5780687352c4da374f41ed1d
Gokul Kadnar <118423597+gokul-kadnar@users.noreply.github.com>
2025-05-27 17:46:17 +0530

    Update MyController.java

--- src/main/java/com/cicd/pipeline/MyController.java
+++ src/main/java/com/cicd/pipeline/MyController.java
@@ -1,5 +1,6 @@
 package com.cicd.pipeline;
 
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
